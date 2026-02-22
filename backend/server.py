a37a0069f64990cdab2953953eaf6aafb92db684
edevs10 <e.goidevs@gmail.com>
2026-02-22 06:11:29 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -24,7 +24,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+   allow_origins=os.environ.get('CORS_ORIGINS', 'http://localhost:3000').split(','),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
