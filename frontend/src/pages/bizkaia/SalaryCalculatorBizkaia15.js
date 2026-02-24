276298e5b42dfbcb1e1855dae66499900bd3f070
edevs10 <e.goidevs@gmail.com>
2026-02-24 20:47:41 +0100

    Update SalaryCalculatorBizkaia15.js

--- frontend/src/pages/bizkaia/SalaryCalculatorBizkaia15.js
+++ frontend/src/pages/bizkaia/SalaryCalculatorBizkaia15.js
@@ -81,11 +81,10 @@ const SalaryCalculatorBizkaia15 = () => {
   };
 
   const handlePrevious = () => {
-    // Si viene de la página de retención variable, volver ahí
-    if (answers?.retention_variable) {
-      navigate('/salary/bizkaia/retention-variable');
-    } else {
+    if (answers?.annual_payments) {
       navigate('/salary/payments-question');
+    } else {
+      navigate('/salary/check');
     }
   };
 
