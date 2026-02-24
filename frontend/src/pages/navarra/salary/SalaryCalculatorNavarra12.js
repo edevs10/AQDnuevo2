3250b8419b3a738f4709a95cc9720f5f2f0da334
edevs10 <e.goidevs@gmail.com>
2026-02-24 20:50:47 +0100

    Update SalaryCalculatorNavarra12.js

--- frontend/src/pages/navarra/salary/SalaryCalculatorNavarra12.js
+++ frontend/src/pages/navarra/salary/SalaryCalculatorNavarra12.js
@@ -69,10 +69,7 @@ const SalaryCalculatorNavarra12 = () => {
   };
 
   const handlePrevious = () => {
-    // Si viene de la página de retención variable, volver ahí
-    if (answers?.retention_variable) {
-      navigate('/salary/navarra/retention-variable');
-    } else if (answers?.annual_payments) {
+    if (answers?.annual_payments) {
       navigate('/salary/payments-question');
     } else {
       navigate('/salary/check');
