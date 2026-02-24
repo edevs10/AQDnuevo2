f180af09d45954b725ad1621a37c716602b5a63a
edevs10 <e.goidevs@gmail.com>
2026-02-24 20:50:16 +0100

    Update SalaryCalculatorAlava15.js

--- frontend/src/pages/alava/SalaryCalculatorAlava15.js
+++ frontend/src/pages/alava/SalaryCalculatorAlava15.js
@@ -80,12 +80,11 @@ const SalaryCalculatorAlava15 = () => {
     }
   };
 
-  const handlePrevious = () => {
-    // Si viene de la página de retención variable, volver ahí
-    if (answers?.retention_variable) {
-      navigate('/salary/alava/retention-variable');
-    } else {
+const handlePrevious = () => {
+    if (answers?.annual_payments) {
       navigate('/salary/payments-question');
+    } else {
+      navigate('/salary/check');
     }
   };
 
